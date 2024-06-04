c6b807db9ff357fb4d86e26c6885cedc545c1d30
Uzma Ilyas <uzma.paec@gmail.com>
2024-06-04 23:14:52 +0500

    insufficient balance added

---
+++ main.js
@@ -0,0 +1,71 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let mybalaance = 100000;
+let mypin = 1415;
+console.log("***ATM MACHINE***");
+const myans = await inquirer.prompt([
+    {
+        name: "pin",
+        type: "number",
+        message: "Enter your pin",
+    },
+]);
+if (myans.pin === mypin) {
+    console.log("\n Congratulation,your pin is correct");
+    for (var i = 0; i < 7; i++) {
+        let myoption = await inquirer.prompt([
+            {
+                name: "choice",
+                type: "list",
+                message: "\n Please select any one of the options",
+                choices: ["Balance", "cashwithdrwl", "SubmitYourBILL"]
+            }
+        ]);
+        if (myoption.choice === "Balance") {
+            console.log(`Your remaining Balance is ${mybalaance} $`);
+            console.log("__________________________________________");
+        }
+        else if (myoption.choice === "cashwithdrwl") {
+            let myamount = await inquirer.prompt([
+                {
+                    name: "amount",
+                    type: "number",
+                    message: "Enter your amount",
+                }
+            ]);
+            mybalaance = mybalaance - myamount.amount;
+            console.log(`Your remaining Balance is ${mybalaance} $`);
+            console.log("__________________________________________");
+        }
+        else if (myoption.choice === "SubmitYourBILL") {
+            let mybillamount = await inquirer.prompt([
+                {
+                    name: "billamount",
+                    type: "number",
+                    message: "Enter your Bill amount",
+                }
+            ]);
+            mybalaance = mybalaance - mybillamount.billamount;
+            console.log(`Your remaining Balance is ${mybalaance} $`);
+            console.log("__________________________________________");
+        }
+        let repeat = await inquirer.prompt([
+            {
+                name: "repeat",
+                message: "\n\n Want to repeat the operation",
+                type: "list",
+                choices: ["Yes", "No"],
+            }
+        ]);
+        if (repeat.repeat === "Yes") {
+            i++;
+        }
+        else if (repeat.repeat === "No") {
+            i = 10;
+            //    console.log("by")
+        }
+    }
+}
+else {
+    console.log("Pin is incorrect.Try again");
+}
